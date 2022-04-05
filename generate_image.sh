9876bcd77c43b6437e19c3f7d8fdcd66317cb499
PeerHerholz <herholz.peer@gmail.com>
2022-04-05 09:43:08 +0200

    update computational environment

--- generate_image.sh
+++ generate_image.sh
@@ -13,9 +13,9 @@ generate_docker() {
              --user=neuroscout\
              --miniconda \
                 conda_install="python=3.8 nb_conda nb_conda_kernels nbconvert==6.4.5 nbformat jupyter" \
-                pip_install='seaborn myst-nb pyns==0.4.8 nilearn==0.8.0 altair nimare==0.0.11
-                             jupytext==1.11.5 ghp-import jupyter-book==0.12.2 myst-nb==0.13.2
-                             sphinx-book-theme jupyter_contrib_nbextensions nbval' \
+                pip_install='seaborn pyns==0.4.10 nilearn==0.8.0 altair nimare==0.0.11 pymare==0.0.2
+                             jupytext==1.11.5 ghp-import jupyter-book==0.12.2 myst-nb==0.13.2 sympy==1.2
+                             sphinx-book-theme jupyter_contrib_nbextensions nbval duecredit' \
                 create_env='neuroscout_paper' \
                 activate=true \
              --copy . /home/neuroscout/paper \
